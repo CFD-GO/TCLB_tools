eaf66de11e16e0e9f2be1614f11b3a4890a829a3
mdzik <dziki01@gmail.com>
2016-08-19 15:19:28 +0200

    clean XML generator, add preatty print

--- Python/CLB/CLBXMLWriter.py
+++ Python/CLB/CLBXMLWriter.py
@@ -146,7 +146,7 @@ class CLBConfigWriter:
 
     def dump(self):
         self.indent(self.root)
-        return ET.tostring(self.root)
+        return ET.tostring(self.root,  pretty_print=True)
 
     def indent(self, elem, level=0):
         i = "\n" + level*"  "
@@ -166,7 +166,7 @@ class CLBConfigWriter:
 
     def write(self, filename):
         tree = ET.ElementTree(element=self.root) 
-        tree.write(filename)
+        tree.write(filename,  pretty_print=True)
 
     def addModelParam(self, name, value):
         n = ET.SubElement(self.model,'Params')
@@ -190,6 +190,7 @@ class CLBConfigWriter:
         
         iterations = _set_by_kw(kwargs, 'iterations', 1)
         vtk = _set_by_kw(kwargs, 'vtk', 0)
+        vtk_what = _set_by_kw(kwargs, 'vtk_fields', 0)
         log = _set_by_kw(kwargs, 'log', 0)
         failcheck = _set_by_kw(kwargs, 'failcheck', 0)
 
@@ -198,23 +199,25 @@ class CLBConfigWriter:
         failcheck_nz = _set_by_kw(kwargs, 'failcheck_nz', 1)
 
         
-        self.model = self.root
+        #self.model = self.root
         n = ET.SubElement(self.root, 'Solve')
         n.set('Iterations', str(iterations))
         if vtk > 0:
             n2 = ET.SubElement(n, 'VTK')
             n2.set('Iterations', str(vtk))
-
+            if vtk_what > 0:
+                n2.set('what', str(vtk_what))       
+                        
         if log > 0:
             n3 = ET.SubElement(n, 'Log')
             n3.set('Iterations', str(log))
         if failcheck > 0:
-            n3 = ET.SubElement(n, 'Failcheck')
-            n3.set('Iterations', str(failcheck))
-            n3.set('nx', str(failcheck_nx))
-            n3.set('ny', str(failcheck_ny))
-            n3.set('nz', str(failcheck_nz))
-            ET.SubElement(n3, 'VTK')                
+            n4 = ET.SubElement(n, 'Failcheck')
+            n4.set('Iterations', str(failcheck))
+            n4.set('nx', str(failcheck_nx))
+            n4.set('ny', str(failcheck_ny))
+            n4.set('nz', str(failcheck_nz))
+            ET.SubElement(n4, 'VTK')                
 ##############
 # ELEMENT METHODE
 #############
@@ -261,53 +264,10 @@ if __name__ == "__main__":
     
     CLBc.addBox(dy=90, fy=-90)
     
-    CLBc.addWall(name="zwall")
-    #CLBc.addSphere(dy=">128", ny="256", dx=">-128", nx="256")
-    CLBc.addSphere(dy="0", ny="256", dx=">-128", nx="256")
-    CLBc.addBox(fy=-1, nx=50)
-    
-    CLBc.addRightSymmetry()
-    CLBc.addBox(fy=-1, dx=-1)
-  
-    CLBc.addPythonInline(nx=50, eval=""" 
-def test(*args, **kwargs):    
-    pass
-    """)
-  
-    CLBc.addTopSymmetry()
-    CLBc.addBox(fx=-1, dy=-1)
-    
-    params = {
-    'InletVelocity': "0.0",
-    'Density':"0.05",
-    'Density-zwet':"3.117355002492964819",
-    'Density-zwall':"2",
-    'Density-zbc':"3.2625",
-    'Temperature':"0.56",
-    'FAcc':"1",
-    'Magic':"0.008",
-    'MagicA':"-0.152",
-    'MagicF':"-0.6666666666666",
-    'GravitationY':"-0.00000",
-    'GravitationX':"-0.00000",
-    'MovingWallVelocity':"0.000",
-    'S0':"0",
-    'S1':"0",
-    'S2':"0",
-    'S3':"-0.333333",
-    'S4':"0",
-    'S5':"0",
-    'S6':"0",
-    'S7':"0.00",
-    'S8':"0.00"
-    }
-    
-    for n in params:
-        CLBc.addModelParam(n, params[n])
-    
+
     CLBc.addSolve(iterations=1, vtk=1)
-    CLBc.addSolve(iterations=100, vtk=50)
+    CLBc.addSolve(iterations=100, vtk=50, failcheck=1)
     
-    CLBc.dump()
+    print CLBc.dump()
    # CLBc.write('/home/michal/tach-17/mnt/fhgfs/users/mdzikowski/yang-laplace-sphere-matrix/test.xml')
    
